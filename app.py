0bbd41e64cd33d8fdbeb7ce8131c8125efc0423d
makhaseck <133665215+makhaseck@users.noreply.github.com>
2025-04-01 20:29:05 +0200

    Update app.py by new task
    i learn the if and else condition

--- app.py
+++ app.py
@@ -15,3 +15,21 @@ name = "Anna"
 surname = "grey"
 student_name = name + " " + surname
 print(student_name)
+#TASK
+#1. write IF ELSE statement to validate if x is larger than y . Return TRUE if YES
+
+def compare(x, y):  # Changer le nom de la fonction pour éviter les conflits
+    if x > y:
+        return True  # Utiliser True avec majuscule
+    return False  # Utiliser False avec majuscule
+
+print(compare(300, 200))
+
+#2. #1. write IF ELSE statement to validate if x is larger than y and less than b. Return TRUE if YES
+
+def compare(x, y, b):  # Changer le nom de la fonction pour éviter les conflits
+    if (x > y) and (x < b):
+        return True  # Utiliser True avec majuscule
+    return False  # Utiliser False avec majuscule
+
+print(compare(300, 200, 500))
