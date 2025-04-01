9d1ab1cb1eee6b152b53d04b0f0bfad7f9f62bb3
makhaseck <133665215+makhaseck@users.noreply.github.com>
2025-04-01 20:04:21 +0200

    Created basis for our first application app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+#simpe Math
+x = 2
+y = 3
+z = x + y
+print(x)
+print(y)
+
+#Python is using indents
+a = 10
+b = 5
+c = a / b
+d = a * b
+e = a % b
+name = "Anna"
+surname = "grey"
+student_name = name + " " + surname
+print(student_name)
